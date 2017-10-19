d1b54fc34adaf3d114d7ba226362211930f250fa
Alexander Komissarov <a.komissarov@g.nsu.ru>
2017-10-19 20:10:20 +0700

    Implemented let, do statements parsing Also: implemented values and constants mapping; fixed operation definition error; changed some models collection types

--- Filomena.Backend/src/Filomena.Backend.Parsing/Maybe.fs
+++ Filomena.Backend/src/Filomena.Backend.Parsing/Maybe.fs
@@ -1,3 +1,17 @@
 namespace Filomena.Backend.Parsing
 
 type ('t, 'e) Maybe = Ok of 't | Failed of 'e
+
+module Maybe = 
+    type MaybeBuilder () = 
+        member this.Return x = Ok x
+
+        member this.ReturnFrom (m: ('a, 'b) Maybe) = m
+
+        member this.Bind (m, f) = 
+            match m with
+            | Ok x -> f x
+            | Failed e -> Failed e
+
+    let maybe = MaybeBuilder ()
+
