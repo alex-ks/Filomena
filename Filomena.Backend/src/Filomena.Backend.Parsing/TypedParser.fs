8dd96f0a68e98d3af95dcfadfa3a72d96045efac
Alexander Komissarov <a.komissarov@g.nsu.ru>
2017-11-28 00:26:15 +0700

    Removed unused code

--- Filomena.Backend/src/Filomena.Backend.Parsing/TypedParser.fs
+++ Filomena.Backend/src/Filomena.Backend.Parsing/TypedParser.fs
@@ -58,16 +58,6 @@ module TypedParser =
             else Some (t.GenericArguments |> Seq.map typeToModel |> Seq.toList) in
         { name = name; parameters = parameters }
 
-    type ExpressionProduct = 
-        | SourceOperation of int // indicates that expression results to some value produced by operation with index returned
-        | Mnemonic of string // indicates that expression results to some known value identified by mnemonic name returned
-        | Operation of string // indicates that expression results to sone known function identified by full entity name
-
-    type ExpressionResult = 
-        | Mnemonic of string
-        | Output of Filomena.Backend.Parsing.Operation
-        | Alias of string
-
     let (|Reversed|) lst = List.rev lst
 
     let uniqueName () = System.Guid.NewGuid () |> sprintf "%A"
@@ -156,89 +146,6 @@ module TypedParser =
 
         | _ ->
             notSupported "Expression is not supported"
-
-    // let rec visitExpression' graph metaValues expr =
-    //     let valueSources, duplicates = metaValues
-    //     let rec escapeName graph metaValues name = 
-    //         let valueSources, duplicates = metaValues
-    //         let containsName map = Map.containsKey name map in
-    //         if graph.mnemonicsTable |> containsName 
-    //             || valueSources |> containsName 
-    //             || duplicates |> containsName 
-    //         then 
-    //             escapeName graph metaValues (name + "'")
-    //         else 
-    //             name
-    //     let bindExpressionName graph metaValues bindVar bindExpr = 
-    //         maybe { let name = escapeName graph metaValues bindVar
-    //                 let! newGraph, product = visitExpression' graph metaValues bindExpr in
-    //                 return 
-    //                     match product with
-    //                     | SourceOperation code ->
-    //                         let updatedSources = valueSources |> Map.add name code in
-    //                         newGraph, (updatedSources, duplicates)
-    //                     | Mnemonic value ->
-    //                         let updatedDuplicates = duplicates |> Map.add name value in
-    //                         newGraph, (valueSources, updatedDuplicates) } in
-    //     match expr with
-    //     | BasicPatterns.Sequential (firstExpr, secondExpr) -> 
-    //         maybe { let! newGraph, firstProd = visitExpression' graph metaValues firstExpr
-    //                 let! lastGraph, secondProd = visitExpression' newGraph metaValues secondExpr in
-    //                 return
-    //                     match firstProd, secondProd with
-    //                     | SourceOperation firstOp, SourceOperation secondOp ->
-    //                         if lastGraph.dependencies.[secondOp] |> Set.contains firstOp then
-    //                             lastGraph, SourceOperation secondOp // TODO: check if opcode is correct
-    //                         else
-    //                             let newDependencies = 
-    //                                 lastGraph.dependencies 
-    //                                 |> List.mapi (fun i deps -> if i = secondOp then Set.add firstOp deps else deps) in
-    //                             { operations = lastGraph.operations; 
-    //                               dependencies = newDependencies; 
-    //                               mnemonicsTable = lastGraph.mnemonicsTable }, SourceOperation secondOp // TODO: check if opcode is correct
-    //                     | _ ->
-    //                         lastGraph, secondProd }
-    //     // TODO: ensure that operations cannot be used as arguments
-    //     | BasicPatterns.Call (objExprOpt, memberOrFuc, typeArgs1, typeArgs2, argExprs) ->
-            
-    //         Failed "Oops"
-                    
-    //         // Failed "Not implemented" // TODO: implement
-    //     | BasicPatterns.Application (funExpr, typeArgs, argExprs) ->
-
-    //         Failed "Not implemented" // TODO: implement
-    //     | BasicPatterns.Let ((bindVar, bindExpr), bodyExpr) -> 
-    //         maybe { let! newGraph, newMetavalues = bindExpressionName graph metaValues bindVar.FullName bindExpr in            
-    //                 return! visitExpression' newGraph newMetavalues bodyExpr }
-    //     | BasicPatterns.Value (value) ->
-    //         match duplicates |> Map.tryFind value.FullName with
-    //         | Some mnemonic ->
-    //             Ok (graph, Mnemonic mnemonic)
-    //         | None ->
-    //             Ok (graph, Mnemonic value.FullName)
-    //     | BasicPatterns.Const (constValObj, constType) ->
-    //         let mnemonicValue = { dataType = typeToModel constType; value = string constValObj } // TODO: check if custom ToString needed
-    //         let mnemonic = uniqueName ()
-    //         let updatedGraph = { operations = graph.operations;
-    //                              dependencies = graph.dependencies;
-    //                              mnemonicsTable = Map.add mnemonic mnemonicValue graph.mnemonicsTable } in
-    //         Ok (updatedGraph, Mnemonic mnemonic)
-    //     | BasicPatterns.NewArray (arrType, initlist) ->
-    //         Failed "Not implemented" // TODO: implement
-    //     | BasicPatterns.NewTuple (tupType, initList) ->
-    //         Failed "Not implemented" // TODO: implement
-    //     | BasicPatterns.TupleGet (tupType, elemIndex, tupleExpr) ->
-    //         Failed "Not implemented" // TODO: implement
-    //     | BasicPatterns.Lambda (lambdaVar, bodyExpr) ->
-    //         Failed "Lambdas are currently unsupported"
-    //     | BasicPatterns.IfThenElse (condExpr, thenExpr, elseExpr) ->
-    //         Failed "Conditions are unsupported until lambda are ready"
-    //     | BasicPatterns.TypeTest (suspectType, exprToTest) ->
-    //         Failed "Type test is currently usupported"
-    //     | BasicPatterns.DefaultValue (defaultType) ->
-    //         Failed "Default values are not supported"
-    //     | _ ->
-    //         Failed "Construction is not supported"
 
     let (|Function|) (x: FSharpMemberOrFunctionOrValue) = x.FullType.IsFunctionType
 
